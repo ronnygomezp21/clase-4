96de40726f669241a724417617e2f4c60658f26b
ronnygomezp21 <ronny.gomezpe@ug.edu.ec>
2026-04-20 14:32:42 -0500

    feat: init repository

---
+++ index.js
@@ -0,0 +1,5 @@
+const app = require('./server.js')
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'route not found' })
+})
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+
+const port = process.env.PORT
+
+app.use(cors())
+
+app.disable('')
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json({ message: 'bienvenido al api de peliculas mas chevere del mundo' })
+})
+
+app.listen(port, () => {
+  console.log(`El api esta corriendo en htttp:\\localhost:${port}`)
+})
+
+module.exports = app
